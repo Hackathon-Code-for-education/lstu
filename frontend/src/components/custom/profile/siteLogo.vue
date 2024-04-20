2c61bbf4413fdd7aa7440f705a897ac2a2dd1a36
Igor Kretov <knigor1337@gmail.com>
2024-04-20 15:24:15 +0300

    added auth page

---
+++ frontend/src/components/custom/profile/siteLogo.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps({
+  isVertical: Boolean // Определяет вид логотипа: вертикальное или горизонтальное отображение
+})
+</script>
+
+<template>
+  <!-- Контейнер для всего лого -->
+  <div
+    class="flex items-center h-[64px] gap-4 w-fit cursor-default"
+    :class="{ 'flex-col': isVertical }"
+  >
+    <!-- Контейнер только для лого -->
+    <div class="flex items-center gap-2">
+      <!-- Прямоугольник -->
+      <div class="flex items-center justify-center h-[64px] w-[128px] rounded-lg bg-slate-600">
+        <p class="text-4xl text-white font-bold">ВУЗ</p>
+      </div>
+      <p class="text-4xl font-bold">Рецензент</p>
+    </div>
+    <!-- <p class="text-lg font-base">Храните знания и учитесь!</p> -->
+  </div>
+</template>
+
+<style>
+.-left-12 {
+  left: -5.7rem;
+}
+</style>
--- frontend/src/views/AuthPage.vue
+++ frontend/src/views/AuthPage.vue
@@ -1,13 +1,62 @@
 <template>
-  <div class="h-full w-full flex justify-center p-6 sm:p-16">
-    <div
-      class="flex flex-col h-full w-full sm:w-1/2 lg:w-1/3 gap-6 sm:gap-16 justify-center items-center"
-    >
-      <h1>Hello,world, test-test</h1>
+  <div class="flex items-center justify-center h-screen">
+    <div class="flex flex-col items-center gap-10">
+      <siteLogo class="mb-1"></siteLogo>
+      <h1 class="text-black font-semibold text-2xl">Вход</h1>
+      <div class="grid w-full max-w-sm items-center gap-1.5">
+        <form class="w-96 space-y-6" @submit="onSubmit">
+          <FormField v-slot="{ componentField: EmailField }" name="email">
+            <FormItem>
+              <FormLabel>Почта</FormLabel>
+              <FormControl>
+                <Input type="text" placeholder="Введите почту..." v-bind="EmailField" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          </FormField>
+          <FormField v-slot="{ componentField: PasswordField }" name="password">
+            <FormItem>
+              <FormLabel>Пароль</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="Введите пароль..." v-bind="PasswordField" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          </FormField>
+
+          <Toaster />
+          <div class="flex flex-col items-center gap-5">
+            <Button type="submit" class="text-white"> Войти </Button>
+            <p @click="goToRegister" class="cursor-pointer">Забыл пароль?</p>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import siteLogo from '@/components/custom/profile/siteLogo.vue'
+import { useRoute, useRouter } from 'vue-router'
+import { Toaster } from '@/components/ui/toast'
+import { useToast } from '@/components/ui/toast/use-toast'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { Label } from '@/components/ui/label'
+import { toTypedSchema } from '@vee-validate/zod'
+import { useForm } from 'vee-validate'
+import * as z from 'zod'
+import { h } from 'vue'
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
+import { ref } from 'vue'
+import axios from 'axios'
+</script>
 
 <style></style>
