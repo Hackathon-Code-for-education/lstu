7554aa1a419fd682d672f96b7a83d6b71f00bb58
Igor Kretov <knigor1337@gmail.com>
2024-04-20 19:22:16 +0300

    fix register

--- backend/app/registration.php
+++ backend/app/registration.php
@@ -4,24 +4,21 @@ header('Access-Control-Allow-Origin: http://localhost:5173');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Access-Control-Allow-Methods: POST');
 
+$pdo = new PDO("pgsql:host=postgres-db; dbname=code-future-2024", "user", "user");
 
-$pdo = new PDO("pgsql:host=postgres-db; dbname=code_future", "user", "user");
-
-if (isset($_POST["register"])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $email = $_POST["email"];
     $full_name = $_POST["full_name"];
     $password = $_POST["password"];
-    $role_user = ($_POST["role"] == "student") ? "abiturient" : "representative"; // Определение роли пользователя
-
+    $role_user = $_POST["role"];
 
     if (empty($email) ||  empty($password) || empty($full_name)) {
-        // Возвращаем ошибку, если не все поля заполнены
+
         header('Content-Type: application/json');
-        echo json_encode(array('status' => 'error', 'message' => 'Заполните все поля и подтвердите согласие на обработку персональных данных.'));
+        echo json_encode(array('status' => 'error', 'message' => 'Заполните все поля'));
         exit();
-    } 
-     else {
+    } else {
 
         $stmtemail = $pdo->prepare("SELECT * FROM users WHERE email_user = ?");
         $stmtemail->execute([$email]);
@@ -30,12 +27,11 @@ if (isset($_POST["register"])) {
         if ($useremail) {
 
             header('Content-Type: application/json');
-            echo json_encode(array('status' => 'error', 'message' => 'Пользователь с таким логином уже существует.'));
+            echo json_encode(array('status' => 'error', 'message' => 'Пользователь с такой почтой уже существует.'));
             exit();
         } else {
             // Хеширование пароля
             $hashedPassword = hash('sha256', $password);
-
 
             $data = [
                 'email' => $email,
@@ -44,20 +40,21 @@ if (isset($_POST["register"])) {
                 'role_user' => $role_user,
             ];
 
+            $data_json = [
+                'email' => $email,
+                'full_name_user' => $full_name,
+                'role_user' => $role_user,
+            ];
 
             $stmtInsert = $pdo->prepare("INSERT INTO users (email_user, password_user, role, full_name_user) VALUES (:email, :hashedPassword, :role_user, :full_name_user)");
             $stmtInsert->execute($data);
 
-
             $id_user = $pdo->lastInsertId();
 
-            // Добавление id_user к данным
-            $data['id_user'] = $id_user;
+            // Добавляем id_user к данным, которые будут отправлены в ответе JSON
+            $data_json['id_user'] = $id_user;
 
-
-            $userDataString = json_encode(array('status' => 'success', 'user' => $data));
-            file_put_contents('dataa.txt', $userDataString . PHP_EOL, FILE_APPEND);
-
+            $userDataString = json_encode(array('status' => 'success', 'user' => $data_json));
 
             header('Content-Type: application/json');
             echo $userDataString;
@@ -65,7 +62,4 @@ if (isset($_POST["register"])) {
         }
     }
 }
-
 ?>
-
-
