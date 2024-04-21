c6a04125dabf5ce45fc9ac2f053582f23dc74477
DmitryErkhov <91343507+DmitryErkhov@users.noreply.github.com>
2024-04-21 15:02:25 +0300

    Change chat

--- frontend/src/components/custom/profile/dialogList.vue
+++ frontend/src/components/custom/profile/dialogList.vue
@@ -1,21 +1,33 @@
 <script setup>
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
 import dialogListElement from './dialogListElement.vue'
+import Student from './student.vue'
 </script>
 
 <template>
   <div class="flex flex-col gap-4">
-    <h3 class="h3">Чат со студентами</h3>
-    <Card class="w-full">
-      <CardHeader>
-        <CardTitle>Список диалогов</CardTitle>
-      </CardHeader>
-      <CardContent class="flex flex-col gap-6">
-        <dialogListElement/>
-        <dialogListElement/>
-        <dialogListElement/>
-        <dialogListElement/>
-      </CardContent>
-    </Card>
+    <h3 class="font-semibold text-3xl">Чат со студентами</h3>
+    <div class="row-dialog flex justify-between">
+      <Card class="w-3/4">
+        <CardHeader>
+          <CardTitle>Диалоги</CardTitle>
+        </CardHeader>
+        <CardContent class="flex flex-col gap-6">
+          <dialogListElement />
+          <dialogListElement />
+          <dialogListElement />
+          <dialogListElement />
+        </CardContent>
+      </Card>
+      <Card class="ml-4 w-1/4">
+        <CardHeader>
+          <CardTitle>Студенты</CardTitle>
+        </CardHeader>
+        <CardContent class="flex flex-col gap-6">
+          <Student />
+          
+        </CardContent>
+      </Card>
+    </div>
   </div>
 </template>
---
+++ frontend/src/components/custom/profile/student.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
+</script>
+
+<template>
+  <div class="flex gap-4">
+    <Avatar>
+      <AvatarImage src="https://api.dicebear.com/8.x/lorelei-neutral/svg" alt="@radix-vue" />
+      <AvatarFallback>CN</AvatarFallback>
+    </Avatar>
+    <!-- Содержимое элемента списка диалогов -->
+    <div class="flex flex-col gap-2">
+      <!-- Заголовок элемента -->
+      <div class="flex justify-between">
+        <p>Аноним №15573</p>
+        <!-- Метаданные об элементе -->
+      </div>
+      <!-- Последняя строка диалога -->
+    </div>
+  </div>
+</template>
